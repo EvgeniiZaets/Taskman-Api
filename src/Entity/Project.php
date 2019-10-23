53176350606d8d80f2409111fdfb3ff6edd0a124
Evgenii Zaets <evgeniizaets@gmail.com>
2019-10-23 21:36:14 +0300

    add new entities; require doctrine migrations bundle

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -1,31 +1,42 @@
 <?php
 
-
 namespace App\Entity;
-
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 
-
 /**
- * Class Project
- * @package App\Entity
- *
- * @ORM\Entity()
  * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\ProjectRepository")
  */
 class Project
 {
     /**
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      */
     private $id;
 
     /**
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string", length=255)
      */
     private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
 }
